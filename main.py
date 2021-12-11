0ce43c9f64c8ce77bad47e170e044892ba2c4f84
Nikita Ostrenkov <xfakehopex@gmail.com>
2021-12-11 20:57:47 +0300

    User and Password args now are required

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     parser.add_argument('url', help='Rocket.Chat server url')
     parser.add_argument('message', help='Message to post')
     parser.add_argument('channel', help='Channel to post message')
-    parser.add_argument('--user', help='Rocket.Chat user name')
-    parser.add_argument('--password', help='Rocket.Chat user password')
+    parser.add_argument('--user', help='Rocket.Chat user name', required=True)
+    parser.add_argument('--password', help='Rocket.Chat user password', required=True)
     parser.add_argument('--alias', help='Rocket.Chat user alias')
 
     args = parser.parse_args()
